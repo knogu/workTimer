907e29b44b221c438aa3d27a468bf3741552397d
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2024-06-08 08:10:10 +0900

    Format

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ import useTimer from "./react-timer-hook/useTimer.ts";
 
 
 function timerString(minutes: number, seconds: number) {
-    return minutes.toString() + ":" + padZero(seconds)
+  return minutes.toString() + ":" + padZero(seconds)
 }
 
 type durationState = {
@@ -36,132 +36,137 @@ type durationState = {
 }
 
 const Timer = () => {
-    const [curSessionStartTime, setCurSessionStartTime] = useRecoilState(currentStartTimeState)
-    const [curDurationType, setCurDurationType] = useRecoilState(currentDurationTypeState)
-    const curPauseDurations = useRecoilValue(curPauseDurationsState)
-    const [todayDoneSessionList, setTodayDoneSessionList] = useRecoilState(todayDoneSessionListState)
-    const [curDurationIdx, setDurationIdx] = useState(0);
-    const settings = useRecoilValue(timerConfigState)
+  const [curSessionStartTime, setCurSessionStartTime] = useRecoilState(currentStartTimeState)
+  const [curDurationType, setCurDurationType] = useRecoilState(currentDurationTypeState)
+  const curPauseDurations = useRecoilValue(curPauseDurationsState)
+  const [todayDoneSessionList, setTodayDoneSessionList] = useRecoilState(todayDoneSessionListState)
+  const [curDurationIdx, setDurationIdx] = useState(0);
+  const settings = useRecoilValue(timerConfigState)
 
-    const {
-        seconds,
-        minutes,
-        isRunning,
-        start,
-        pause,
-        resume,
-        restart,
-    } = useTimer(
-        () => {
-            if (curDurationType === DurationType.Focus) {
-              const doneSession: Session = {
-                startTime: curSessionStartTime!,
-                endTime: new Date(),
-                pauseDurations: curPauseDurations,
-              }
-
-              addSessionToDb(doneSession);
-              setTodayDoneSessionList((prev) => [...prev, doneSession])
-            }
-
-            Push.create(getPushMsg(curDurationType), {
-              body: getPushMsg(curDurationType),
-              timeout: 4000,
-              onClick: function () {
-                window.focus();
-              }
-            });
-
-            setCurSessionStartTime(null);
-
-
-            let nextMinutes: number;
-
-          const nextDurationIdx = (curDurationIdx + 1) % settings.focusCntBeforeLongBreak
-          if (nextDurationIdx == 2 * settings.focusCntBeforeLongBreak - 1) {
-            nextMinutes = settings.longBreakLength
-            setCurDurationType(DurationType.LongBreak)
-          } else if (nextDurationIdx % 2 == 0) {
-            nextMinutes = settings.focusLength
-            setCurDurationType(DurationType.Focus)
-          } else {
-            nextMinutes = settings.shortBreakLength
-            setCurDurationType(DurationType.ShortBreak)
+  const {
+    seconds,
+    minutes,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer(
+      () => {
+        if (curDurationType === DurationType.Focus) {
+          const doneSession: Session = {
+            startTime: curSessionStartTime!,
+            endTime: new Date(),
+            pauseDurations: curPauseDurations,
           }
 
-          const time = new Date();
-          time.setSeconds(time.getSeconds() + nextMinutes * 3);
-          restart(time, false)
+          addSessionToDb(doneSession);
+          setTodayDoneSessionList((prev) => [...prev, doneSession])
+        }
 
-            setDurationIdx(() => nextDurationIdx)
+        Push.create(getPushMsg(curDurationType), {
+          body: getPushMsg(curDurationType),
+          timeout: 4000,
+          onClick: function () {
+            window.focus();
+          }
+        });
 
-        },
-    );
+        setCurSessionStartTime(null);
 
-    function onStart() {
-        setCurSessionStartTime(new Date());
-        start();
-    }
 
-    function startOrResume() {
-        return (
-            curSessionStartTime == null ?
-                onStart
-                :
-                resume
-        )
-    }
+        let nextMinutes: number;
 
-    if(!Push.Permission.has()) {
-        Push.Permission.request(()=>{}, ()=>{});
-    }
+        const nextDurationIdx = (curDurationIdx + 1) % settings.focusCntBeforeLongBreak
+        if (nextDurationIdx == 2 * settings.focusCntBeforeLongBreak - 1) {
+          nextMinutes = settings.longBreakLength
+          setCurDurationType(DurationType.LongBreak)
+        } else if (nextDurationIdx % 2 == 0) {
+          nextMinutes = settings.focusLength
+          setCurDurationType(DurationType.Focus)
+        } else {
+          nextMinutes = settings.shortBreakLength
+          setCurDurationType(DurationType.ShortBreak)
+        }
 
-    useEffect(() => {
-        getTodaySessions().then((sessions) => setTodayDoneSessionList(sessions))
-    }, []);
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + nextMinutes * 3);
+        restart(time, false)
 
+        setDurationIdx(() => nextDurationIdx)
+
+      },
+  );
+
+  function onStart() {
+    setCurSessionStartTime(new Date());
+    start();
+  }
+
+  function startOrResume() {
     return (
-        <div className="contents">
-            <div className="graph-container">
-                {RecordsBar()}
-            </div>
-            <div>
-                <div className="timer-container">
-                    <div className="time">
-                        {timerString(minutes, seconds)}
-                    </div>
+        curSessionStartTime == null ?
+            onStart
+            :
+            resume
+    )
+  }
 
-                    <div>{
-                      curDurationType === DurationType.Focus ?
-                          "Focus"
-                          :
-                          "Break"
-                    }</div>
+  if (!Push.Permission.has()) {
+    Push.Permission.request(() => {
+    }, () => {
+    });
+  }
 
-                    <div>
-                        {
-                            isRunning ?
-                                <button className="timer-button" onClick={pause}><i className="fa fa-pause"></i></button>
-                                :
-                                <button className="timer-button" onClick={startOrResume()}><i className="fa fa-play"></i></button>
-                        }
+  useEffect(() => {
+    getTodaySessions().then((sessions) => setTodayDoneSessionList(sessions))
+  }, []);
 
-                    </div>
-
-                    <div><p>total {displayedMinutes(sessionMinutesSum(todayDoneSessionList))} / {displayedMinutes(settings.goalMinutesPerDay)} ({(sessionMinutesSum(todayDoneSessionList) / settings.goalMinutesPerDay * 100).toFixed(0)} %)</p></div>
-                </div>
-            </div>
+  return (
+      <div className="contents">
+        <div className="graph-container">
+          {RecordsBar()}
         </div>
+        <div>
+          <div className="timer-container">
+            <div className="time">
+              {timerString(minutes, seconds)}
+            </div>
+
+            <div>{
+              curDurationType === DurationType.Focus ?
+                  "Focus"
+                  :
+                  "Break"
+            }</div>
+
+            <div>
+              {
+                isRunning ?
+                    <button className="timer-button" onClick={pause}><i className="fa fa-pause"></i></button>
+                    :
+                    <button className="timer-button" onClick={startOrResume()}><i className="fa fa-play"></i>
+                    </button>
+              }
+
+            </div>
+
+            <div>
+              <p>total {displayedMinutes(sessionMinutesSum(todayDoneSessionList))} / {displayedMinutes(settings.goalMinutesPerDay)} ({(sessionMinutesSum(todayDoneSessionList) / settings.goalMinutesPerDay * 100).toFixed(0)} %)</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
-    );
+  );
 }
 
 export const TimerPage = () => {
-    return (
-        <>
-            <Header/>
-            {Timer()}
-        </>
-    )
+  return (
+      <>
+        <Header />
+        {Timer()}
+      </>
+  )
 }
