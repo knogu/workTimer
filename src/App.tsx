fcfbe16bbee17b3fac545af3d92b2ea757998cf0
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2024-06-11 21:05:56 +0900

    Confirm when users try to leave

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,23 @@ import {RecordsBar} from "./RecordsBar.tsx";
 import {Timer} from "./Timer.tsx";
 import {Goals} from "./Goals.tsx";
 import AchievedGoals from "./Achievements.tsx";
+import {useEffect} from "react";
 
 export const TimerPage = () => {
+
+  const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+    // todo: prevent only when focus is ongoing
+    e.preventDefault();
+    e.returnValue = "Fine to leave page ?";
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
   return (
       <>
         <Header />
